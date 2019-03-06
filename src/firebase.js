595528048ecd628f83368768e710b24f0f2b49fe
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-03-06 11:07:22 -0600

    inicio de sesion con correo electronico con firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+let app_firebase = {};
+(function(){
+    var config = {
+        apiKey: "AIzaSyA-br4fjgN3TqUQgfE-Y2eGzfdajBuwa_Q",
+        authDomain: "red-social-laboratoriamx.firebaseapp.com",
+        databaseURL: "https://red-social-laboratoriamx.firebaseio.com",
+        projectId: "red-social-laboratoriamx",
+        storageBucket: "red-social-laboratoriamx.appspot.com",
+        messagingSenderId: "727465925051"
+    };
+    firebase.initializeApp(config);
+    app_firebase = firebase;
+})()
