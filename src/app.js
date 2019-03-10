c0a5f99b5fecf563a8cbde313ac643dcdf6fb9fd
AcheZeta <ache.mariana@gmail.com>
2019-03-09 18:55:53 -0600

    Login con materialize funcionando ok y redirigiendo a main

--- src/app.js
+++ src/app.js
@@ -1,71 +1,5 @@
-
-/*var config = {
-  apiKey: "AIzaSyA-br4fjgN3TqUQgfE-Y2eGzfdajBuwa_Q",
-  authDomain: "red-social-laboratoriamx.firebaseapp.com",
-  databaseURL: "https://red-social-laboratoriamx.firebaseio.com",
-  projectId: "red-social-laboratoriamx",
-  storageBucket: "red-social-laboratoriamx.appspot.com",
-  messagingSenderId: "727465925051"
-};
-firebase.initializeApp(config);
-
-/*(function(){
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyA-br4fjgN3TqUQgfE-Y2eGzfdajBuwa_Q",
-  authDomain: "red-social-laboratoriamx.firebaseapp.com",
-  databaseURL: "https://red-social-laboratoriamx.firebaseio.com",
-  projectId: "red-social-laboratoriamx",
-  storageBucket: "red-social-laboratoriamx.appspot.com",
-  messagingSenderId: "727465925051"
-};
-firebase.initializeApp(config);
-const txtEmail = document.getElementById('txtEmail');
-const txtPassword = document.getElementById('txtPassword');
-const btnLogin = document.getElementById('btnLogin');
-const btnSingUp = document.getElementById('btnSignUp');
-const btnLogout = document.getElementById('btnLogout');
-
-btnLogin.addEventListener('click', e => {
-const email = txtEmail.value;
-const pass = txtPassword.value;
-const auth = firebase.auth();
-const promise = auth.signInWithEmailAndPassword(email,pass);
-promise.catch(e => console.log(e.message));
-});
-
-btnSingUp.addEventListener('click', e => {
-const email = txtEmail.value;
-const pass = txtPassword.value;
-const auth = firebase.auth();
-const promise = auth.createUserWithEmailAndPassword(email,pass);
-promise
-.catch(e => console.log(e.message));
-});
-
-btnLogout.addEventListener('click' , e => {
-  firebase.auth().signOut();
-})
-
-firebase.auth().onAuthStateChanged(firebaseUser => {
-  if(firebaseUser) {
-    console.log(firebaseUser);
-    btnLogout.classList.remove('hide');
-  } else {
-    console.log('not logged in');
-    btnLogout.classList.add('hide');
-  }
-})
-
-
-
-}());*/
-
-
-
-
-const txtEmail = document.getElementById('txtEmail');
-const txtPassword = document.getElementById('txtPassword');
+const txtEmail = document.getElementById('email-login');
+const txtPassword = document.getElementById('password-login');
 const btnLogin = document.getElementById('btnLogin');
 const btnSingUp = document.getElementById('btnSignUp');
 
@@ -75,53 +9,53 @@ btnSingUp.addEventListener('click', e => {
   console.log(email)
   const pass = txtPassword.value;
   firebase.auth().createUserWithEmailAndPassword(email, pass)
-  .then(function() {
-    verify() 
-  })
-  .catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  })
-  });
+    .then(function () {
+      verify()
+    })
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    })
+});
 
-  function verify() {
-    var user = firebase.auth().currentUser;
-  
-  user.sendEmailVerification().then(function() {
+function verify() {
+  var user = firebase.auth().currentUser;
+
+  user.sendEmailVerification().then(function () {
     // Email sent.
     console.log('sending email');
-  }).catch(function(error) {
+  }).catch(function (error) {
     // An error happened.
     console.log(error);
   });
-  }
+}
 
-const txtEmail2 = document.getElementById('txtEmail2');
-const textPassword2 = document.getElementById('txtPassword2');
+const txtEmail2 = document.getElementById('email-register');
+const textPassword2 = document.getElementById('password-register');
 
-  btnLogin.addEventListener('click', e => {
-    const email2 = txtEmail2.value;
-    const pass2 = textPassword2.value;
-    console.log(email2, pass2);
-    firebase.auth().signInWithEmailAndPassword(email2, pass2).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode);
-      console.log(errorMessage);
+btnLogin.addEventListener('click', e => {
+  const email2 = txtEmail2.value;
+  const pass2 = textPassword2.value;
+  console.log(email2, pass2);
+  firebase.auth().signInWithEmailAndPassword(email2, pass2).catch(function (error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
 
-      //inner watchercontraseña o correo invalido
-    });
+    //inner watchercontraseña o correo invalido
   });
+});
 
 function watcher() {
-  firebase.auth().onAuthStateChanged(function(user) {
+  firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      if(user.emailVerified == true) {
+      if (user.emailVerified == true) {
         window.location.replace('main.html');
         console.log('main.html')
-      } 
-      if(user.emailVerified == false) {
-      console.log('verifica tu correo') 
+      }
+      if (user.emailVerified == false) {
+        console.log('verifica tu correo')
       }
       // User is signed in.
       var displayName = user.displayName;
@@ -139,7 +73,8 @@ function watcher() {
       console.log('no existe usuario activo');
     }
   });
-}watcher();
+}
+watcher();
 
 // funcion para entar a pagina principal (main.html)
 /*
@@ -150,14 +85,29 @@ function loged(){
   }*/
 
 //Carrusel
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems);
+});
 
 //Modal1
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
 
+//Boton enviar desde el modal
+
+const bttnLogout = document.getElementById("bttn-logout")
+bttnLogout.addEventListener('click', function logOut() {
+    //pop up de confirmación
+    firebase.auth().signOut()
+      .then(function () {
+        console.log('saliendo..')
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }),
+  index.logOut
+()
