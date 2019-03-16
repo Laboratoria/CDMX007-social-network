04a60f22cd516de0a7ea4bfe0d74a59c967f0927
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-03-15 18:09:19 -0600

    css nav cambio de color

--- src/app.js
+++ src/app.js
@@ -230,7 +230,7 @@ const btnPost = document.getElementById('btn-post')
 btnPost.addEventListener('click', saveDataInPostColection => {
   const txtPost = document.getElementById('txtPost')
   var post = txtPost.value;
-  db.collection("post").add({
+  db.collection("posts").add({
     post: post
   })
     .then(function (docRef) {
@@ -244,7 +244,7 @@ btnPost.addEventListener('click', saveDataInPostColection => {
 
 /*leer documento firestone*/
 var showPost = document.getElementById('showPost');
-db.collection("post").onSnapshot((querySnapshot) => {
+db.collection("posts").onSnapshot((querySnapshot) => {
   showPost.innerHTML= "";
   querySnapshot.forEach(function(doc) {
       // doc.data() is never undefined for query doc snapshots
@@ -283,7 +283,7 @@ function editPost(id, post){
 
 /*elimianr post*/
 function removePost(id){ 
-  db.collection("post").doc(id).delete().then(function() {
+  db.collection("posts").doc(id).delete().then(function() {
     console.log("Document successfully deleted!");
   }).catch(function(error) {
     console.error("Error removing document: ", error);
