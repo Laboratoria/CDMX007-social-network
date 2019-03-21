247c42da64fb9f69df5f0e693ad6d28f213aecc1
karen-mtz <dkareen.mtz@gmail.com>
2019-03-21 17:00:13 -0600

    filtro para publico y privado

--- src/interactividad.js
+++ src/interactividad.js
@@ -12,6 +12,12 @@ const interactividad = () => {
        var elems = document.querySelectorAll('.modal');
        var instances = M.Modal.init(elems);
      });
+
+     document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('select');
+        var instances = M.FormSelect.init(elems);
+      });
+
 inter_es = {
     cancel: 'Cancelar',
     clear: 'Limpiar',
