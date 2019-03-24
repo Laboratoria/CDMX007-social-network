5a52f17383569e4cc3eee73bb64ccd70121da7f5
Cristalgarcia <solocrissgar@gmail.com>
2019-03-24 02:25:08 -0600

    Contiene el registro e ingreso para guardar la información en la base de datos de firebase y verificación de correo

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,2 @@
 // Initialize Firebase
   // TODO: Replace with your project's customized code snippet
-  var config = {
-    apiKey: "AIzaSyBDn6Sq7DcRWZjZemvgi_bHHavRBR_7cn8",
-    authDomain: "social-network-3cf6e.firebaseapp.com",
-    databaseURL: "https://social-network-3cf6e.firebaseio.com",
-    projectId: "social-network-3cf6e",
-    storageBucket: "social-network-3cf6e.appspot.com",
-    messagingSenderId: "1066953533782"
-  };
-  firebase.initializeApp(config);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,99 @@
-var firebase = require("firebase/app");
+/*var firebase = require("firebase/app");
 require("firebase/auth");
-require("firebase/firestore");
+require("firebase/firestore");*/
+function signIn(){
+    const emailSignIn=  document.getElementById("emailSignIn").value;
+    const passwordSignIn= document.getElementById("passwordSignIn").value;
+
+    firebase.auth().createUserWithEmailAndPassword(emailSignIn, passwordSignIn)
+    .then (function(){
+        verification()
+    })
+
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        
+        // ...
+      });
+}
+
+function logIn(){
+    const emailLogIn= document.getElementById("emailLogIn").value;
+    const passwordLogIn= document.getElementById("passwordLogIn").value;
+
+    firebase.auth().signInWithEmailAndPassword(emailLogIn, passwordLogIn)
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+      });
+}
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log("existe usuario activo")
+           // aparece(user);
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          console.log("*******");
+          console.log(user.emailVerified)
+          console.log("*******");
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          console.log(user);
+          // ...
+        } else {
+          // User is signed out.
+          console.log("no existe usuario activo")
+          // ...
+        }
+      });
+     
+
+
+      /*function aparece(user){
+          const user = user;
+          const contenido = document.getElementById("content");
+          if(user.emailVerified){
+          contenido.innerHTML=`
+          <button onclick="cerrar()">Cerrar sesión</button>`;
+          }
+        }*/
+
+
+       /* firebase.auth().signOut()
+        .then(function(){
+            console.log("saliendo...")
+
+        })
+        .catch(function(error){
+         console.log(error)
+        })*/
+
+
+      function verification(){
+        var user = firebase.auth().currentUser;
+
+        user.sendEmailVerification().then(function() {
+          // Email sent.
+          console.log("Enviando correo")
+        }).catch(function(error) {
+          // An error happened.
+          console.log(error);
+        });
+      }
+
+
+
+
+//var db = firebase.firestore();
+
